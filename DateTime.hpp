f71041a4ccdddb0184ceebca87e9b59107486e6e
Ivan Savin <68004563+SoV3n04ek@users.noreply.github.com>
2021-12-16 19:09:22 +0200

    added realisation class DateTime

--- DateTime.hpp
+++ DateTime.hpp
@@ -8,14 +8,38 @@ enum MONTH_NAMES {
 };
 
 class DateTime {
-	int month;
-	int day;
-	int year;
+private:
+	short month;
+	short day;
+	short year;
+	short hours;
+	short mins; // minutes
 public:
-	DateTime(int month, int day, int year)
+	DateTime(short day = 0, short month = 0, short year = 0,
+				short hours = 0, short mins = 0)
 	:
-		month(0), day(0), year(0)
-	{
-			
-	}
+		day(day), month(month), year(year),
+		hours(hours), mins(mins)
+	{ };
+
+	DateTime(const DateTime& other)
+	:
+		day(other.getDay()),
+		month(other.getMonth()),
+		year(other.getYear()),
+		hours(other.getHours()),
+		mins(other.getMins())
+	{ };
+
+	short getMonth() const { return month; }
+	short getDay()   const { return day;   }
+	short getYear()  const { return year;  }
+	short getHours() const { return hours; }
+	short getMins()  const { return mins;  }
+
+	DateTime& setMonth(short month)  { this->month = month; return *this; }
+	DateTime& setDay(short day)		 { this->day = day;		return *this; }
+	DateTime& setYear(short year)	 { this->year = year;	return *this; }
+	DateTime& setHours(short hours)  { this->hours = hours; return *this; }
+	DateTime& setMinutes(short mins) { this->mins = mins;	return *this; }
 };
